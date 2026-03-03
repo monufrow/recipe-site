1bbda1b398a6c745c818e788b4276da061193f72
Monufrow <159821928+monufrow@users.noreply.github.com>
2026-03-02 23:17:20 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// View engine
+app.set('view engine', 'ejs');
+
+// Routes
+const recipeRoutes = require('./routes/recipes');
+app.use('/', recipeRoutes);
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
